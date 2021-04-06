0dec38e7a3f17352a9b1001642b0a36043c96799
Mark Freeman <dorsettri@gmail.com>
2021-04-06 11:03:08 +0100

    add gatsby transformer blog posts and sort posts in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,82 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
   return (
     <Layout>
-      <h1>Amazing Pandas Eating Things</h1>
       <div>
-        <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4
+          css={css`
+            margin-bottom: 2em;
+          `}
+        >
+          {data.allMarkdownRemark.totalCount} Posts
+        </h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div
+            css={css`
+              margin-bottom: 2em;
+            `}
+            key={node.id}
+          >
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p
+              css={css`
+                font-size: 0.6rem;
+                color: #d1d;
+                margin-bottom: 0;
+              `}
+            >
+              Time to read: {node.timeToRead}min
+            </p>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
