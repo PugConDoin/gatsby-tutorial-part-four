22fcac557b4e058b76fcceac4e835d573f4a2525
Mark Freeman <dorsettri@gmail.com>
2021-04-07 18:44:02 +0100

    add onCreateNode createPages blog-post.js gatsby-node.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/react"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default function Home({ data }) {
-  console.log(data)
   return (
     <Layout>
       <div>
@@ -17,44 +16,32 @@ export default function Home({ data }) {
         >
           Amazing Pandas Eating Things
         </h1>
-        <h4
-          css={css`
-            margin-bottom: 2em;
-          `}
-        >
-          {data.allMarkdownRemark.totalCount} Posts
-        </h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div
-            css={css`
-              margin-bottom: 2em;
-            `}
-            key={node.id}
-          >
-            <h3
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
+              <h3
                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p
-              css={css`
-                font-size: 0.6rem;
-                color: #d1d;
-                margin-bottom: 0;
-              `}
-            >
-              Time to read: {node.timeToRead}min
-            </p>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #555;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -73,8 +60,10 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
           }
+          fields {
+            slug
+          }
           excerpt
-          timeToRead
         }
       }
     }
